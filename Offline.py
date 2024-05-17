f8372e5cdb55ec367cd50c129f752885ac6a64d7
Yaman Qassas <yaman102011@gmail.com>
2024-05-18 00:42:10 +0300

    Convert words to numbers

--- Offline.py
+++ Offline.py
@@ -5,6 +5,10 @@ import unicodedata
 import re
 import pandas as pd
 import contractions
+
+from num2words import num2words
+from text_to_num import alpha2digit
+
 from spellchecker import SpellChecker
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -16,7 +20,7 @@ from nltk.stem import WordNetLemmatizer, PorterStemmer
 
 
 corpus = {
-	"0": "I'm used to doing this in history class, and that's why I'd won't don't cannot do it with my eyes closed!",
+	"0": "63rd, On twenty-third, I'm used one hundred fifty six to being 1st first second third one two three in history class. Especially in nineteen fifty one, and that's why I'd won't don't cannot do it with my eyes closed!",
 	"1": "it was used in landing craft during world war ii and is used today in private boats and training facilities the 6 71 is an inline six cylinder diesel engine the 71 refers to the displacement in cubic inches of each cylinder the firing order of the engine is 1 5 3 6 2 4 the engine s compression ratio is 18 7 1 with a 4 250 inch bore and a 5 00 inch stroke the engine weighs and is 54 inches long 29 inches wide and 41 inches tall at 2 100 revolutions per minute the engine is capable of producing 230 horse power 172 kilowatts v type versions of the 71 series were developed in 1957 the 6 71 is a two stroke engine as the engine will not naturally aspirate air is provided via a roots type blower however on the 6 71t models a turbocharger and a supercharger are utilized fuel is provided by unit injectors one per cylinder the amount of fuel injected into the engine is controlled by the engine s governor the engine cooling is via liquid in a water jacket in a boat cool external water is pumped into the engine",
 	"2": "after rejecting an offer from cambridge university she moved to london in 1954 working in a mayfair advertising agency while moonlighting as a hat check girl in the night club le club contemporain while working at the royal college of art she met the painter frank bowling when he was still a student there they married in 1960 and had one son kitchen was one of the women interviewed by nell dunn in talking to women 1965 after divorcing bowling in the late sixties kitchen went on to live with and later marry the writer dulan barber continuing to write novels she also began writing non fiction with biographies of patrick geddes and gerard manley hopkins in later life she bought a house in barnwell northamptonshire which became the subject of her book of the same name she died on 23 november 2005 the novelist bessie head was a close friend the pair corresponded from 1969 until head s death in 1986 on a range of subjects including head s novel a question of power",
 	"3": "mat zan coached kuala lumpur fa in 1999 and won malaysia fa cup in 1999 and charity cup in 2000 in 2001 mat zan then move as head coach of terengganu fa and he won the 2001 malaysia cup 2001 malaysia charity cup and second place in malaysia league with plus f c in 2008 he took 2nd place in premier league and automatically promoted to malaysia super league in 2012 he comeback with terengganu fa and go through to fa cup semi finals and round two in afc cup in 2015 mat zan move to melaka united and won 2015 malaysia fam league and won 2016 malaysia premier league after his contract was not renewed with melaka at the end of 2016 he was appointed as new head coach of airasia allstars fc which was renamed as petaling jaya rangers f c for the 2017 season he signed a 1 year contract with negeri sembilan form the 2019 season mat zan also coached the club side of malaysia u 21 squad harimau muda b who won plate winner singapore starhub league cup thanj nien newspaper cup vietnam 3rd place and hassanal bolkiah cup brunei 3rd place"
@@ -33,6 +37,12 @@ class TextProcessing:
 
 		# Expand contractions, eg: "cannot, I'm" => "can not, I am"
 		txt = TextProcessing.expand_contractions(txt)
+
+		# Separates numbers from words or other characters
+		txt = TextProcessing.sep_num_words(txt)
+
+		# Convert words to numbers
+		txt = TextProcessing.words_to_num(txt)
 
 		# Remove punctuations
 		txt = txt.translate(str.maketrans('', '', string.punctuation))
@@ -85,6 +95,27 @@ class TextProcessing:
 	def expand_contractions(cls, text):
 		return contractions.fix(text)
 	
+	# Separates numbers from words or other characters, excluding ordinal numbers.
+	@classmethod
+	def sep_num_words(cls, text):
+		return re.sub(r"(?<!\d)(\d+)(st|nd|rd|th)", r" \1\2", text).strip()
+
+	# Convert words to numbers
+	@classmethod
+	def words_to_num(cls, text):
+		return alpha2digit(text, "en", True, ordinal_threshold=0)
+
+	# Convert Numbers to Words
+	@classmethod
+	def num_to_words(cls, text):
+		after_spliting = text.split()
+
+		for index in range(len(after_spliting)):
+			if after_spliting[index].isdigit():
+				after_spliting[index] = num2words(after_spliting[index])
+		numbers_to_words = ' '.join(after_spliting)
+		return numbers_to_words
+
 	@classmethod
 	def correct_spelling(cls, tokens: List[str]) -> List[str]:
 		spell = SpellChecker()
@@ -122,12 +153,14 @@ class TextProcessing:
 		stemmed_words = [stemmer.stem(word) for word in words]
 		return stemmed_words
 
-
+# Create VSM Index
 # vectorizer = TfidfVectorizer(preprocessor=TextProcessing.process, tokenizer=word_tokenize)
 # tfidf_matrix = vectorizer.fit_transform(corpus.values())
 # df = pd.DataFrame(tfidf_matrix.toarray(), columns=vectorizer.get_feature_names_out(), index=corpus.keys())
 # print(df.to_string(max_cols=10))
 
+
+# Print corpus after text processing
 new_corpus = {}
 for key, value in corpus.items():
 	value = TextProcessing.process(value)
